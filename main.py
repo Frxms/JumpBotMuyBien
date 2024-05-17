a4b4d3ced2312bbce4c603afa933a2dc7e9666c0
Lars1406 <lars@larzeax.com>
2024-05-17 17:56:01 +0200

    push weil lags amk

--- main.py
+++ main.py
@@ -1,6 +1,45 @@
+import random
 import re
 from util.engine import getAllMoves
 import pydevd_pycharm
+
+
+def generateFEN(board, move, turn):
+    letter_to_index = {
+        "A": 0,
+        "B": 1,
+        "C": 2,
+        "D": 3,
+        "E": 4,
+        "F": 5,
+        "G": 6,
+        "H": 7
+    }
+    move = move.split(" - ")
+    move[0] = move[0].replace(move[0][0], str(letter_to_index[move[0][0]]))
+    move[1] = move[1].replace(move[1][0], str(letter_to_index[move[1][0]]))
+    board[int(move[0][0])][int(move[0][1]) + 1] = ""
+    board[int(move[1][0])][int(move[1][1]) + 1] = turn + str(board[int(move[1][0])][int(move[1][1])])
+    print(board)
+    FEN_rows = []
+    for row in board:
+        rows = ""
+        empty = 0
+        for column in row:
+            if column == "X":
+                continue
+            elif column == "":
+                empty += 1
+            else:
+                if empty > 0:
+                    rows += str(empty)
+                    empty = 0
+                rows += column + "0"
+        if empty > 0:
+            rows += str(empty)
+        FEN_rows.append(rows)
+
+    return "/".join(FEN_rows) + " " + turn
 
 
 def createVis(FEN):
@@ -24,38 +63,47 @@ def createVis(FEN):
                 if index2 == len(line) - 1:
                     finished.append('X')
             elif index == 7:
-                    if index2 == len(line) - 1:
-                        finished.append('X')
+                if index2 == len(line) - 1:
+                    finished.append('X')
         boardArray.append(finished)
     return boardArray
 
 
 def calcMove(board, turn):
-    alph = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
     possibleMoves = []
+    startingPoints = []
     for i, row in enumerate(board):
         for j, column in enumerate(row):
             if board[i][j] != "" and board[i][j][-1] == turn:
                 possibleMoves.append(getAllMoves(i, j, turn, board))
                 print(possibleMoves)
-                for moves in possibleMoves:
-                    for move in moves:
-                        print(f"{alph[int(j)]}{int(i)} - {alph[move[1]], move[0]}")
-                #possibleMoves.extend([f"{int(i)}-{int(j)}, {move}" for move in getAllMoves(i, j, turn, board)])
-                # possibleMoves.append(f"{int(i)}-{int(j)} :: {getAllMoves(i, j, turn, board)}")
-                #.append(f"{int(i)}-{int(j)}, {move}" for move in getAllMoves(i, j, turn, board))
+                startingPoints.append([j, i])
+    possibleMoves2 = []
+    for i, moves in enumerate(possibleMoves):
+        for move in moves:  # startingPoints[i][0-1] move[0]move[2]
+            #print(f"{alph[startingPoints[i][0]]}{num[startingPoints[i][1]]} - {move[0]}{move[2]}")
+            #print(f"{alph[startingPoints[i][0]]}{num[startingPoints[i][1]]} - {alph[int(move[2])]}{num[int(move[0])]}")
+            print(refactor_to_readable(startingPoints, i, move))
+
     return possibleMoves
 
-#try:
-FEN = "r01r0r01r0/1r0rr1r0r0r01/3r04/2b05/4r0b02/8/1b0b0b0b0b0b01/1b0b0b0b01 r"
-splitted = FEN.split(" ")
-turn = splitted[1]
-board = createVis(splitted[0])
-print(board)
-print(calcMove(board, turn))
-# except Exception as e:
-#     print(f"An exception occurred: {e}")
-#     pydevd_pycharm.settrace('localhost', port=12345, stdoutToServer=True, stderrToServer=True, suspend=True)
-#     raise
+
+def refactor_to_readable(startingPoints, index, move):
+    alph = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    num = ['8', '7', '6', '5', '4', '3', '2', '1']
+    return f"{alph[startingPoints[index][0]]}{num[startingPoints[index][1]]} - {alph[int(move[2])]}{num[int(move[0])]}"
 
 
+if __name__ == "__main__":
+    FEN = "1r0r0r01r0/1r0rr1r0r0r01/3r04/2b05/4r0b02/8/1b0b0b0b0b0b01/1b0b0b0b01 b"  #36 Züge
+    a = [['X', '', 'r', 'r', 'r', '', 'r', 'X'], ['', 'r', 'rr', '', 'r', 'r', 'r', ''],
+         ['', '', '', 'r', '', '', '', ''], ['', '', 'b', '', '', '', '', ''], ['', '', '', '', 'r', 'b', '', ''],
+         ['', '', '', '', '', '', '', ''], ['', 'b', 'b', 'b', 'b', 'b', 'b', ''],
+         ['X', '', 'b', 'b', 'b', 'b', '', 'X']]
+    print(generateFEN(a, "C5 - C6", "r"))
+    FEN2 = "3b02/2b05/1b06/1r0rr2b01/8/5r02/1r0r03b01/3r02 b"  #15 Züge
+    splitted = FEN.split(" ")
+    turn = splitted[1]
+    board = createVis(splitted[0])
+    print(board)
+    calcMove(board, turn)
--- util/engine.py
+++ util/engine.py
@@ -34,16 +34,17 @@ def diaCalc(row, field, board, turn):
             moves.append([row - 1, field + 1])
     return moves
 
+
 # true means legal move
 def checkFront(row, field, board, turn):
     if turn == "r":
-        if board[row + 1][field] != turn or len(board[row + 1][field]) > 1:
+        if board[row + 1][field] != "" and (len(board[row + 1][field]) > 1 or board[row + 1][field] != turn):
             return False
         if board[row + 1][field] == "X":
             return False
         return [row + 1, field]
     if turn == "b":
-        if board[row - 1][field] != turn or len(board[row - 1][field]) > 1:
+        if board[row - 1][field] != "" and (len(board[row - 1][field]) > 1 or board[row - 1][field] != turn):
             return False
         if board[row - 1][field] == "X":
             return False
@@ -53,32 +54,37 @@ def checkFront(row, field, board, turn):
 def checkSides(row, field, board, turn, sides):
     if sides:
         #check if figure on the left and not whoppable or enemy figure or out of board
-        if board[row][field - 1] != turn or len(board[row][field - 1]) > 1 or field - 1 < 0 or board[row][field - 1] == "X":
+        if board[row][field - 1] != "" and (
+                board[row][field - 1] != turn or len(board[row][field - 1]) > 1 or field - 1 < 0 or board[row][
+            field - 1] == "X"):
             return False
     else:
         # check if figure on the right and not whoppable or enemy figure or out of board
-        if board[row][field + 1] != turn or len(board[row][field + 1]) > 1 or field + 1 >= len(board[row]) or board[row][field + 1] == "X":
+        if board[row][field + 1] != "" and (
+                board[row][field + 1] != turn or len(board[row][field + 1]) > 1 or field + 1 >= len(board[row]) or
+                board[row][field + 1] == "X"):
             return False
 
     return True
 
+
 def twoUp(row, field, board, turn):
     moves = []
     # red
-    if turn == "br" or turn == "rr":
-        if (legalMoveCheck(board[row + 2, field + 1], turn)
-                and (field + 1 < len(board[row + 2]) or not board[row + 2, field + 1] == "X")):
+    if turn == "r":
+        if (legalMoveCheck(board[row + 2][field + 1], turn)
+                and (field + 1 < len(board[row + 2]) or not board[row + 2][field + 1] == "X")):
             moves.append([row + 2, field + 1])
-        if (legalMoveCheck(board[row + 2, field - 1], turn)
-                and (field - 1 >= 0 or not board[row + 2, field - 1] == "X")):
+        if (legalMoveCheck(board[row + 2][field - 1], turn)
+                and (field - 1 >= 0 or not board[row + 2][field - 1] == "X")):
             moves.append([row + 2, field - 1])
     # blue
-    elif turn == "rb" or turn == "bb":
-        if (legalMoveCheck(board[row - 2, field - 1], turn)
-                and (field - 1 >= 0 or not board[row - 2, field - 1] == "X")):
+    elif turn == "b":
+        if (legalMoveCheck(board[row - 2][field - 1], turn)
+                and (field - 1 >= 0 or not board[row - 2][field - 1] == "X")):
             moves.append([row - 2, field - 1])
-        if (legalMoveCheck(board[row - 2, field + 1], turn)
-                and (field + 1 < len(board[row - 2]) or not board[row - 2, field + 1] == "X")):
+        if (legalMoveCheck(board[row - 2][field + 1], turn)
+                and (field + 1 < len(board[row - 2]) or not board[row - 2][field + 1] == "X")):
             moves.append([row - 2, field + 1])
     return moves
 
@@ -86,22 +92,25 @@ def twoUp(row, field, board, turn):
 def hardTurn(row, field, board, turn):
     moves = []
     # red
-    if turn == "br" or turn == "rr":
-        if (legalMoveCheck(board[row + 1, field + 2], turn)
-                and (field + 2 < len(board[row + 1]) or not board[row + 1, field + 2] == "X")):
+    if turn == "r":
+        print(board[row + 2][field + 1])
+
+        if (legalMoveCheck(board[row + 1][field + 2], turn)
+                and (field + 2 < len(board[row + 1]) or not board[row + 1][field + 2] == "X")):
             moves.append([row + 1, field + 2])
-        if (legalMoveCheck(board[row + 1, field - 2], turn)
-                and (field >= 0 or not board[row + 1, field - 2] == "X")):
+        if (legalMoveCheck(board[row + 1][field - 2], turn)
+                and (field >= 0 or not board[row + 1][field - 2] == "X")):
             moves.append([row + 1, field - 2])
     # blue
-    elif turn == "rb" or turn == "bb":
-            if (legalMoveCheck(board[row - 1, field - 2], turn)
-                    and (field - 2 >= 0 or not board[row - 1, field - 2] == "X")):
-                moves.append([row - 1, field - 2])
-            if (legalMoveCheck(board[row - 1, field + 2], turn)
-                    and (field + 2 < len(board[row - 1]) or not board[row - 1, field + 2] == "X")):
-                moves.append([row - 1, field + 2])
+    elif turn == "b":
+        if (legalMoveCheck(board[row - 1][field - 2], turn)
+                and (field - 2 >= 0 or not board[row - 1][field - 2] == "X")):
+            moves.append([row - 1, field - 2])
+        if (legalMoveCheck(board[row - 1][field + 2], turn)
+                and (field + 2 < len(board[row - 1]) or not board[row - 1][field + 2] == "X")):
+            moves.append([row - 1, field + 2])
     return moves
+
 
 def legalMoveCheck(targetField, turn):
     if turn == "b":
@@ -121,6 +130,8 @@ def legalMoveCheck(targetField, turn):
 
 def getAllMoves(row, field, turn, board):
     moves = []
+    alph = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    num = ['8', '7', '6', '5', '4', '3', '2', '1']
     currentField = board[row][field]
     # normal Figure
     if currentField != "X" and len(currentField) <= 1:
@@ -128,7 +139,7 @@ def getAllMoves(row, field, turn, board):
         diag = diaCalc(row, field, board, turn)
         left = checkSides(row, field, board, turn, True)
         right = checkSides(row, field, board, turn, False)
-        if not front:
+        if front:  # TODO front False obwohl er nach unten kann ? edge case?
             moves.append(front)
         for i in diag:
             moves.append(i)
@@ -142,8 +153,7 @@ def getAllMoves(row, field, turn, board):
             moves.append(move)
         for move in twoUp(row, field, board, turn):
             moves.append(move)
-    print(moves)
-    formatted_moves = [(f"{move[0]}-{move[1]}") for move in moves]
+    #print(moves)
+    formatted_moves = []
+    formatted_moves = [f"{move[0]}-{move[1]}" for move in moves]
     return formatted_moves
-
-
